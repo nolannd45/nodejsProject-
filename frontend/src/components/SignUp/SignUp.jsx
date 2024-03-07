671cf9542fc69a00a2ce97212d95a0d4734ed1ab
nolannd45 <nolannd45@gmail.com>
2024-03-07 04:10:27 +0100

    test cicd25

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -40,6 +40,7 @@ const Signup = () => {
       <section className="h-full">
         <div className="container px-6 pt-20">
           <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+            {/* <!-- Left column container with background--> */}
             <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
               <img
                 src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
