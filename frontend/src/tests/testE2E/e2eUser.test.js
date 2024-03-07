8c27438013eba36116c097f70c4d6c665b5694e6
erwan-c <104755539+erwan-c@users.noreply.github.com>
2024-03-07 20:12:13 +0100

    e2e reservation ticket en coursv2

--- frontend/src/tests/testE2E/e2eUser.test.js
+++ frontend/src/tests/testE2E/e2eUser.test.js
@@ -13,35 +13,28 @@ describe("Test E2E de l'application", function () {
     await driver.quit();
   });
 
-  // Vérifie si la page d'accueil se charge correctement
-  it("devrait charger la page d'accueil", async function () {
-    await driver.get("http://localhost:3000");
-    const pageTitle = await driver.getTitle();
-    // Utilisez expect() ici
-    (await import("chai")).expect(pageTitle).to.equal("Hotel-react");
-  });
-
   // Teste la connexion avec succès
   it("devrait se connecter avec succès", async function () {
     await driver.get("http://localhost:3000/login");
     await driver.findElement(By.id("pseudo")).sendKeys("nolannd");
     await driver.findElement(By.id("password")).sendKeys("nolannd");
     await driver.findElement(By.id("connexion")).click();
-  });
-  // Teste la connexion avec succès
-  it("devrait selectionner et reserver un hôtel", async function () {
-    await driver.get("http://localhost:3000");
     const listHotels = await driver.findElements(By.tagName('a'));
     await listHotels[0].click();
-    await driver.findElement(By.id('reserver')).click();
-    await driver.findElement(By.id("startDate")).sendKeys("2024-03-27T00:00:00.000+00:00");
-    await driver.findElement(By.id("EndDate")).sendKeys("2024-03-29T00:00:00.000+00:00");
-    await driver.findElement(By.id("valider")).click();
+    await driver.get("http://localhost:3000/hotels/65e5041e3dbbdc5ea66a1237");
+    await driver.findElement(By.xpath(`//*[@id="reserver"]`)).click();
 
+    await sleep(2000);
+
+    await driver.findElement(By.xpath(`//*[@id="startDate"]`)).sendKeys("2024-03-27T00:00:00.000+00:00");
+    await driver.findElement(By.xpath(`//*[@id="EndDate"]`)).sendKeys("2024-03-29T00:00:00.000+00:00");
+    await driver.findElement(By.xpath(`//*[@id="valider"]`)).click();
   });
-  // Teste la connexion avec succès
-  // it('devrait Reserver', async function() {
-  //     await driver.get('http://localhost:3000/hotels/65e4cbd12d34b38342b18af9');
-  //     await driver.findElement(By.id('reserver')).click();
+
+  // Teste la reservation
+  // it("devrait selectionner et reserver un hôtel", async function () {
+  //   await driver.get("http://localhost:3000");
+  //   c
+
   // });
 });
