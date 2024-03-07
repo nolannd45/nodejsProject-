17810ba15ef0eb48719206482169a700e9241337
nolannd45 <nolannd45@gmail.com>
2024-03-07 22:04:43 +0100

    test cicd

--- frontend/src/tests/mainHome.test.jsx
+++ frontend/src/tests/mainHome.test.jsx
@@ -1,17 +1,29 @@
-import MainHome from '../components/MainHome';
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Modal from '../components/Modal';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-describe('MainHome Component', () => {
-  test('renders main title correctly', () => {
-    render(
-      <Router>
-        <MainHome />
-      </Router>
-    );
-
-    const mainTitle = screen.getByText("Selectionner l'hôtel de vos rêves!");
-    expect(mainTitle).toBeInTheDocument();
+describe('Modal Component', () => {
+  it('renders the modal button with the correct text', () => {
+    const text = 'Reserver';
+    const { getByText } = render(
+    <Router>
+      <Modal text={text} />
+    </Router>);
+    const modalButton = getByText(text);
+    expect(modalButton).toBeInTheDocument();
   });
 
+  it('hides the modal content when button is clicked again', () => {
+    const text = 'Reserver';
+    const { getByText, queryByText } = render(
+    <Router>
+      <Modal text={text} />
+  </Router>);
+    const modalButton = getByText(text);
+    fireEvent.click(modalButton);
+    fireEvent.click(modalButton);
+    const modalContent = queryByText('Modal Content'); // Replace 'Modal Content' with your actual modal content text
+    expect(modalContent).toBeNull();
+  });
 });
