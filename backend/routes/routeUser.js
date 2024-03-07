8664d8de0621e50fb69aa29c07db8d271d55f044
nolannd45 <nolannd45@gmail.com>
2024-03-07 02:52:32 +0100

    test cicd25

--- backend/routes/routeUser.js
+++ backend/routes/routeUser.js
@@ -9,6 +9,6 @@ routeUser.post("/create",createUser); //dont need connexion
 routeUser.delete("/delete/:id",token ,delUser);//connexion + self or admin only
 routeUser.get("/read",token ,readUser); //connexion + employee only
 routeUser.get("/readById/:id",token ,readById); //connexion + employee only
-routeUser.patch("/update/:id",token, updateUser);//connexion + self or admin only
+routeUser.patch("/update/:id",token, updateUser);//connexion + self or admin onl
 
 export default routeUser;
